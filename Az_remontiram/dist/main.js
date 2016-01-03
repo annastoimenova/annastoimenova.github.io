2b6b492bf93ba9a3c2ee838e58273fbdd1fe340a
annastoimenova <annadragieva@abv.bg>
2016-01-03 22:53:05 +0200

    java_tasks

---
+++ Az_remontiram/dist/main.js
@@ -0,0 +1,9 @@
+var output = "";
+var input = ["soft", "intellect", "academy", 2015];
+
+for (var i = 0; i < input.length; i++) {
+	console.log("adding", input [i]);
+	console.log("input index", i);
+	output = output + input[i];
+};
+console.log("output",output)
